d082aeed56c9de95d7d1d9eca63a16cc617419b3
Doorlabh Panjwani <doorlabhp@gmail.com>
2011-05-04 21:35:16 +0530

    problem with simple cart - line_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,21 @@
 class Product < ActiveRecord::Base
   
+  has_many :line_items
+  
   default_scope :order => 'title'
   validates :title, :description, :image_url, :presence => true 
   validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
   validates :title, :uniqueness => true
-  validates :image_url, :format => { :with => %r{\.gif|png|jpg}i, :message => 'Must be a URL for GIF,JPG or PNG'} 
+  validates :image_url, :format => { :with => %r{\.gif|png|jpg}i, :message => 'Must be a URL for GIF,JPG or PNG'}
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      true
+    else
+      errors.add(:base, 'Line Items present')
+      false
+    end
+  end
 end
